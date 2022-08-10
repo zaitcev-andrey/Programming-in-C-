1738fba4e116bcb86ad8e9f48b8a7457b8b051e9
Andrey Zaitsev <megaandrey525@gmail.com>
2022-08-10 10:23:27 +0300

    all done

---
+++ PracticalWork_1.4/PracticalWork_1.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork_1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!!!");
+            ReadKey();
+
+            Write("Hello");
+            Write(" World");
+            Write("!!!");
+
+            ReadLine();
+        }
+    }
+}
