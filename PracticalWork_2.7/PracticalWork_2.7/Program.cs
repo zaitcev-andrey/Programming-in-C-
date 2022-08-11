4933caaa2d7f21ffc8b73b272f07310a27a2d8e2
Andrey Zaitsev <megaandrey525@gmail.com>
2022-08-11 16:48:15 +0300

    add 2 files

---
+++ PracticalWork_2.7/PracticalWork_2.7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork_2._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
