aa5ea1526191c14048d2dbd043d3b185199af8c1
Andrey Zaitsev <megaandrey525@gmail.com>
2022-08-14 15:09:13 +0300

    done task 1

--- PracticalWork_2.7/PracticalWork_2.7/Program.cs
+++ PracticalWork_2.7/PracticalWork_2.7/Program.cs
@@ -10,6 +10,22 @@ namespace PracticalWork_2._7
     {
         static void Main(string[] args)
         {
+            string fullName = "Ivanov Ivan Ivanovich";
+            int age = 22;
+            string email = "myEmail@gmail.com";
+            double programmScores = 4.2;
+            double mathScores = 4.6;
+            double physicsScores = 4.4;
+
+            string pattern = "Ф.И.О: {0} \nВозраст: {1} \nПочта: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+
+            Console.WriteLine(pattern,
+                fullName,
+                age,
+                email,
+                programmScores,
+                mathScores,
+                physicsScores);
         }
     }
 }
