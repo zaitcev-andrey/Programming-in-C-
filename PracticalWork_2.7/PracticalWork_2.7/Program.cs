835abc100ca26bf926e8f1acfce7fcfcd7d78f05
Andrey Zaitsev <megaandrey525@gmail.com>
2022-08-15 09:23:19 +0300

    fixed the string and add ReadKey

--- PracticalWork_2.7/PracticalWork_2.7/Program.cs
+++ PracticalWork_2.7/PracticalWork_2.7/Program.cs
@@ -17,7 +17,12 @@ namespace PracticalWork_2._7
             double mathScores = 4.6;
             double physicsScores = 4.4;
 
-            string pattern = "Ф.И.О: {0} \nВозраст: {1} \nПочта: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+            string pattern = "Ф.И.О: {0} \n" +
+                "Возраст: {1} \n" +
+                "Почта: {2} \n" +
+                "Баллы по программированию: {3} \n" +
+                "Баллы по математике: {4} \n" +
+                "Баллы по физике: {5}";
 
             Console.WriteLine(pattern,
                 fullName,
@@ -31,6 +36,8 @@ namespace PracticalWork_2._7
             double scoresSum = programmScores + mathScores + physicsScores;
             double arithmeticMean = scoresSum / 3;
             Console.WriteLine($"\nСреднее арифметическое баллов = {arithmeticMean}");
+
+            Console.ReadKey(true);
         }
     }
 }
