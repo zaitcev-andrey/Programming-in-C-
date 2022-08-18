3a8afebda35ad001881e99a1d2ad0b1974129e7d
Andrey Zaitsev <megaandrey525@gmail.com>
2022-08-18 09:49:44 +0300

    add task 3

---
+++ PracticalWork_3.10/task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int num = int.Parse(Console.ReadLine());
+            
+            bool flag = false;
+            while (!flag)
+            {
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag)
+                    Console.WriteLine($"Число {num} это непростое число");
+                else
+                {
+                    Console.WriteLine($"Число {num} это простое число");
+                    flag = true;
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
