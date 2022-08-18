661d9b45c65877de687b8f0b3fdd78a3c2d05f96
Andrey Zaitsev <megaandrey525@gmail.com>
2022-08-18 10:42:32 +0300

    add task4 and task5

---
+++ PracticalWork_3.10/task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minValue = int.MaxValue;
+            Console.Write("Введите длину последовательности чисел: ");
+            int length = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= length; i++)
+            {
+                Console.Write($"Введите число {i}: ");
+                int value = int.Parse(Console.ReadLine());
+                if(value < minValue)
+                    minValue = value;
+            }
+            Console.WriteLine($"Минимальное значение в вашей " +
+                $"последовательности = {minValue}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ PracticalWork_3.10/task5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите максимальное целое число диапазона: ");
+            int maxRange = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            int randValue = random.Next(maxRange + 1);
+
+            Console.Write("Попробуйте отгадать загаданное число." +
+                " Если устанете играть, отправьте пустую строку." +
+                    " Введите число: ");
+            string str;
+            int ourValue;
+            int count = 0;
+            while (true)
+            {
+                str = Console.ReadLine();
+                if (String.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine($"Жаль, что вы не угадали." +
+                        $" Это было число: {randValue}");
+                    break;
+                }
+                ourValue = int.Parse(str);
+                count++;
+                if (ourValue > randValue)
+                    Console.Write("Ваше число больше загаданного," +
+                        " ищите дальше: ");
+                else if(ourValue < randValue)
+                    Console.Write("Ваше число меньше загаданного," +
+                        " ищите дальше: ");
+                else
+                {
+                    Console.WriteLine($"Вы угадали число, это {randValue}");
+                    Console.WriteLine($"Вы угадали с {count} раза!");
+                    break;
+                }
+
+            }
+            
+            Console.ReadKey(true);
+        }
+    }
+}
