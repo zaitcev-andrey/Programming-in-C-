0360feeb86d4137362626148cbce8f434c6632e1
Andrey Zaitsev <megaandrey525@gmail.com>
2022-08-18 09:25:27 +0300

    add task 1 and task 2

--- PracticalWork_3.10/task1/Program.cs
+++ PracticalWork_3.10/task1/Program.cs
@@ -10,6 +10,13 @@ namespace task1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целое число: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine($"Число {number} чётное");
+            else
+                Console.WriteLine($"Число {number} нечётное");
+            Console.ReadKey(true);
         }
     }
 }
---
+++ PracticalWork_3.10/task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет дорогой друг, " +
+                "сколько карт у тебя на руках? Введи это число:");
+            int num = int.Parse(Console.ReadLine());
+            
+            int sum = 0;
+            bool flag = true;
+            while (flag)
+            {
+                sum = 0;
+                Console.WriteLine($"Сейчас вам нужно будет ввести" +
+                $" номинал для каждой карты. " +
+                $"Если это число, вводите от 2 до 10 " +
+                $"включительно.\nЕсли это картинка, то вводите так: " +
+                $"Валет = J, Дама = Q, Король = K, Туз = T");
+                for (int i = 1; i <= num; i++)
+                {
+                    Console.Write($"Введите номинал {i} карты: ");
+                    string str = Console.ReadLine();
+                    switch (str)
+                    {
+                        case "2":
+                        case "3":
+                        case "4":
+                        case "5":
+                        case "6":
+                        case "7":
+                        case "8":
+                        case "9":
+                        case "10":
+                            sum += int.Parse(str);
+                            break;
+                        case "J":
+                        case "Q":
+                        case "K":
+                        case "T":
+                            sum += 10;
+                            break;
+                        default:
+                            Console.WriteLine("\nВы неверно ввели номинал карты, попробуйте снова!\n");
+                            flag = false;
+                            break;
+                    }
+                    if (!flag)
+                        break;
+                }
+                if (!flag)
+                    flag = true;
+                else
+                    flag = false;
+            }
+
+            Console.WriteLine($"\nСумма из {num} карт равна: {sum}");
+            Console.ReadKey(true);
+        }
+    }
+}
